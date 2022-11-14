0ed12c6f8de515c444267fad91dcde5d2e40bf9e
Simax <simaxme@gmail.com>
2022-11-14 16:26:29 +0100

    update

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,14 +1,38 @@
 module.exports = {
   new: [
+    {
+      header: "Das Overlay kann nun neugestartet werden.",
+      content: "Es gibt drei Möglichkeiten, das Overlay nun neuzustarten: \n- Unten Rechts in der Symbolleiste\n- Unten links im Launcher neben der Anzahl der Spieler, die Online sind\n- Rechtsklick auf die Instanz > Overlay neustarten",
+      advanced: {}
+    }
   ],
   improvements: [
+    {
+      header: "Mods werden nun über ihre Hash-Werte validiert.",
+      content: "Das heißt für die Hacker unter euch: Ihr könnt nicht einfach eine Mod mit dem gleichen Dateinamen in den Ordner packen :D",
+      advanced: {}
+    }
   ],
   bugfixes: [
     {
-      header: 'Fix.',
+      header: 'Mods mit einem ungültigen Namen werden automatisch gelöscht.',
       content: '@DerSimeon',
       advanced: {}
-      //advanced: { cm: '391dd9cc', pr: '1412' }
+    },
+    {
+      header: 'Das Argument `fabric.addmods` wurde als Startoption ausgeschlossen.',
+      content: '@DerSimeon',
+      advanced: {}
+    },
+    {
+      header: 'Das Umbenennen von Instanzen funktioniert nun wieder.',
+      content: '',
+      advanced: {}
+    },
+    {
+      header: 'Das Starten von Instanzen funktioniert wieder.',
+      content: '',
+      advanced: {}
     }
   ]
 };
